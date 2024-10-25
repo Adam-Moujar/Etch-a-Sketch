e42ad68e82beb052fbf7e5eec20bde409bf08bb6
Adam Moujar Bakhti <amoujarbakhti@gmail.com>
2024-10-25 10:57:51 +0100

    Finish erase and reset functionality

--- javascript.js
+++ javascript.js
@@ -1,18 +1,81 @@
 const container_div = document.querySelector(".container")
-for(let i = 0; i < 16 * 16; i++){
-    const div = document.createElement("div");
-    div.classList.add("grid_square");
-    div.style.backgroundColor = "gray";
-    div.style.height = "28px";
-    div.style.width = "28px";
-    div.style.border = "1px solid white"
-    div.addEventListener("mouseover", function(e){
-        div.style.backgroundColor="black";
-    });
-    div.addEventListener("mouseleave", function(e){
-        div.style.backgroundColor = "gray";
-    })
+let mouse_down = false;
+document.body.onmousedown = () => (mouse_down = true)
+document.body.onmouseup = () => (mouse_down = false)
 
-    container_div.appendChild(div);
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "grey";
+const DEFAULT_BUTTON_COLOR = "black";
+const DEFAULT_MODE = 'color'
+
+const eraser_button = document.querySelector("#eraser");
+const reset_button = document.querySelector("#reset");
+
+let current_mode = DEFAULT_MODE;
+
+function init_grid(size){
+    for(let i = 0; i < size * size; i++){
+        const grid_tile = document.createElement("div");
+        grid_tile.classList.add("grid_tile");
+        grid_tile.style.backgroundColor = "gray";
+        // ill move these to a css later
+        grid_tile.style.height = "28px";
+        grid_tile.style.width = "28px";
+        grid_tile.style.border = "1px solid white"
+        grid_tile.setAttribute("draggable", "false");
+        grid_tile.style.userSelect = "none";
+        // up
+        grid_tile.addEventListener("mouseover", color_tile);
+        grid_tile.addEventListener("mousedown", color_tile);
+        grid_tile.addEventListener("mouseleave", color_tile);
+
+        container_div.appendChild(grid_tile);
+    }
 }
-console.log(container_div)
+
+function color_tile(e){
+    if(current_mode === "color"){
+        if(e.type === "mousedown" || e.type === "mouseover" && mouse_down) {
+            e.target.style.backgroundColor = DEFAULT_BUTTON_COLOR;
+            e.target.removeEventListener("mouseleave", color_tile);
+        }
+        else if(e.type === "mouseover"){
+            e.target.style.backgroundColor = DEFAULT_BUTTON_COLOR;
+        }
+        else{
+            e.target.style.backgroundColor = DEFAULT_COLOR;
+        }
+    }
+    else if(current_mode === "eraser"){
+        if(e.type === "mousedown" || e.type === "mouseover" && mouse_down) {
+            e.target.style.backgroundColor = DEFAULT_COLOR;
+            e.target.addEventListener("mouseleave", color_tile);
+        }
+    }
+}
+
+function init_buttons(){
+    eraser_button.addEventListener("click", toggle_eraser);
+    reset_button.addEventListener("click", reset_grid);
+}
+
+function toggle_eraser(e){
+    if(!(current_mode === "eraser")){
+        current_mode = "eraser";
+        e.target.style.backgroundColor = "grey";
+    }
+    else{
+        current_mode = DEFAULT_MODE;
+        e.target.style.backgroundColor = "white";
+    }
+}
+
+function reset_grid(e){
+    container_div.replaceChildren();
+    init_grid(DEFAULT_SIZE);
+}
+
+window.onload = () => {
+    init_grid(DEFAULT_SIZE);
+    init_buttons();
+}
